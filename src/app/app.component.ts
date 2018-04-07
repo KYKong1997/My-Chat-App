aafd03aecc8b6704a95fda8b68a6ffe7b4055e1b
KYKong1997 <kongky-wa15@student.tarc.edu.my>
2018-04-07 13:34:38 +0800

    coomit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,23 +7,38 @@ import { AngularFirestore,AngularFirestoreModule } from 'angularfire2/firestore'
   styleUrls: ['./app.component.css']
 }) 
 export class AppComponent {
+
+  //Variable declaration
   title = 'my chat app';
   testing = 'This is a cheat hahahahahahhahahahhahah';
 
   messages=[];
 
+
 name:string;
 message:string;
 
 constructor(public db:AngularFirestore){
+  console.log('line 1');
+  const observer=this.db.collection('Chat').valueChanges();
+  console.log('line 2');
+  observer.subscribe(function(data){
+    console.log(data);
+    console.log('line 3');
+  });
+  console.log('line 4');
+  
 
 }
   sendMessage(){
     //alert('my name is '+this.name+" and the message is "+this.message);
+    //create new object
     const message={
       name:this.name,
-      message:this.message
+      message:this.message,
+      createdOn:new Date()
     };
+    //add item to firebase
     this.db.collection('Chat').add(message);
     //this.messages.push(message);
 
