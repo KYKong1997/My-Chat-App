4ceb0f49b4e4f8442cfc2b81df7cf04542daae1a
KYKong1997 <kongky-wa15@student.tarc.edu.my>
2018-04-07 12:01:31 +0800

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { AngularFirestore,AngularFirestoreModule } from 'angularfire2/firestore';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
-})
+}) 
 export class AppComponent {
-  title = 'app';
+  title = 'my chat app';
+  testing = 'This is a cheat hahahahahahhahahahhahah';
+
+  messages=[];
+
+name:string;
+message:string;
+
+constructor(public db:AngularFirestore){
+
+}
+  sendMessage(){
+    //alert('my name is '+this.name+" and the message is "+this.message);
+    const message={
+      name:this.name,
+      message:this.message
+    };
+    this.db.collection('Chat').add(message);
+    //this.messages.push(message);
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { FormsModule } from '@angular/forms';
+import { AngularFireModule } from 'angularfire2';
+import {AngularFirestoreModule} from 'angularfire2/firestore';
 import { AppComponent } from './app.component';
 
-
+const config = {
+  apiKey: "AIzaSyAwTevbDhXIfxoRir0reiBC0RyNzh5uOFM",
+  authDomain: "my-chat-app-c5559.firebaseapp.com",
+  databaseURL: "https://my-chat-app-c5559.firebaseio.com",
+  projectId: "my-chat-app-c5559",
+  storageBucket: "my-chat-app-c5559.appspot.com",
+  messagingSenderId: "783495393701"
+};
 @NgModule({
+  
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AngularFireModule.initializeApp(config),
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
